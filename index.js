b94ce0a9eaf024efb036d2dc367257a8291fec31
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2024-08-07 20:50:29 -0500

    Ejercicios de javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+// ✏️ Actividad: Sumando números 
+const numero1 = Number(prompt("Ingrese primer número"));
+const numero2 = Number(prompt("Ingrese segundo número"));
+const suma =numero1 +numero2;
+const mensajeSuma = "El resultado de la suma es "+suma;
+alert(mensajeSuma);
+
+// ✏️ Actividad: Realizando más operaciones
+const promedio = suma / 2;
+const triple = promedio * 3;
+const resultadoMenosDiez = triple - 10;
+const mensajeFinal = `El resultado promedio de la suma es :  ${promedio}.
+El resultado triple del promedio es : ${triple}.
+El resultado Menos Diez es : ${resultadoMenosDiez}.
+`;
+
+alert(mensajeFinal);
+
+
+
